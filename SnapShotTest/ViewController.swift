da4a9f68b74683dea15f541bbb65b32013e59cdb
Eva Madarasz <tora.buke@gmail.com>
2020-09-12 01:15:34 +0200

    Initial Commit

---
+++ SnapShotTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapShotTest
+//
+//  Created by Eva Madarasz on 12.09.20.
+//  Copyright © 2020 Eva Madarasz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
