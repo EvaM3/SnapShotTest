99fbae0eb12380a0497d8d7d31a5804f7d39c8c9
Eva Madarasz <tora.buke@gmail.com>
2021-01-23 09:41:45 +0100

    MainViewController is done

--- SnapShotTest/MainViewController.swift
+++ SnapShotTest/MainViewController.swift
@@ -14,8 +14,7 @@ class MainViewController: UIViewController {
     @IBOutlet var cutOut: UIView!
     @IBOutlet var canvasImage: UIImageView!
     @IBOutlet var startButton: UIButton!
-    var imageArray: [UIImage]? = nil
-    var originalImage = [UIImage]()
+    var originalImage = UIImage()
     
     
     
@@ -27,30 +26,19 @@ class MainViewController: UIViewController {
         if segue.identifier == "PlayfieldSegue" {
             
             if let dest = segue.destination as? PlayfieldViewController {
-                if let imageArrayUnwrapped = self.imageArray {
-                    dest.imageArray = imageArrayUnwrapped
-                }
-                
-                if segue.identifier == "PlayfieldSegue" {
-                    let destinationVC = segue.destination as! PlayfieldViewController
-                    destinationVC.imageArray = originalImage
-                    
-                }
-
+                dest.originalImage = originalImage
             }
         }
     }
-
-  
+    
+    
     
     @IBAction func startButton(_ sender: Any) {
         
         let screenshot = self.view.takeScreenshot()
         let croppedImage =  screenshot.cropImage(toRect: cutOut.frame)
-        self.imageArray = croppedImage?.splitImage(row: 4, column: 4)
+        originalImage = croppedImage ?? UIImage()
         self.performSegue(withIdentifier: "PlayfieldSegue", sender: nil)
-        let originalImage = croppedImage
-       
         
     }
 }
